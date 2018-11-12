e3c6ce4b15f5d1866006e09b47195f88f1cebb40
Daria Zubkova <dariaz.2410@gmail.com>
2018-11-12 23:00:45 +0200

    the sum purchases

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        //Scanner scanner = new Scanner(System.in);
 
         Fruit redApple = new Fruit(FruitType.APPLE, Color.RED, 0.146, 10.49);
         System.out.println("redApple: " + redApple);
@@ -19,20 +18,21 @@ public class Main {
         // копия ссылки redApple
         Fruit redApple2 = redApple;
 
-        Fruit greenApple = new Fruit(FruitType.APPLE);
+        Fruit greenApple = new Fruit(FruitType.APPLE, Color.GREEN, 0.240, 11.99);
         System.out.println("greenApple: " + greenApple);
 
-        Fruit lemon = new Fruit(FruitType.LEMON);
+        Fruit lemon = new Fruit(FruitType.LEMON, Color.YELLOW, 0.124, 31.99);
         System.out.println("lemon: " + lemon);
 
-        Fruit banana = null;
+        Fruit banana = new Fruit(FruitType.BANANA, Color.YELLOW, 0.198, 18.99);
         //banana.setType(FruitType.APPLE);
         System.out.println("banana: " + banana);
 
-        Fruit[] arr = new Fruit[3];
+        Fruit[] arr = new Fruit[4];
         arr[0] = redApple;
         arr[1] = greenApple;
         arr[2] = lemon;
+        arr[3] = banana;
 
         printPrice(arr);
 
@@ -43,7 +43,8 @@ public class Main {
         for (int i = 0; i < arr.length; i++) {
             price += arr[i].price();
         }
-        System.out.println("Sum price: " + price);
+        String price2 = String.format("%.2f", price);
+        System.out.println("Sum price: " + price2);
     }
 
 }
